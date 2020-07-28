b141ce22f96d48fdcdb5e034e1244eef190dc2b4
Zhi Wei Gan <zhiwei_gan@hotmail.com>
2020-07-28 11:16:23 +0800

    pypi

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
   packages = ['pySongStructure'],   
   version = '0.1',      
   license='MIT',       
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'Zhi Wei Gan',                   # Type in your name
-  author_email = 'zgan@mit.edu',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  description = 'A high level song structure analysis library using the MSAF Library',   
+  author = 'Zhi Wei Gan',                
+  author_email = 'zgan@mit.edu',   
+  url = 'https://github.com/zhiweigan/pySongStructure',   
+  download_url = 'https://github.com/zhiweigan/pySongStructure/archive/v0.1.tar.gz',   
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
+  install_requires=[            
           'numpy',
           'librosa',
           'msaf',
@@ -18,7 +18,7 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',     
     'Intended Audience :: Developers',     
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3.7',
   ],
 )
