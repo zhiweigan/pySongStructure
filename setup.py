c93b90776e990f06563e3214ce8f99b26718ce0b
Zhi Wei Gan <zhiwei_gan@hotmail.com>
2020-07-28 11:32:14 +0800

    versioning fix

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from distutils.core import setup
 setup(
   name = 'pySongStructure',         
   packages = ['pySongStructure'],   
-  version = '0.1',      
+  version = '0.3',      
   license='MIT',       
   description = 'A high level song structure analysis library using the MSAF Library',   
   author = 'Zhi Wei Gan',                
   author_email = 'zgan@mit.edu',   
   url = 'https://github.com/zhiweigan/pySongStructure',   
-  download_url = 'https://github.com/zhiweigan/pySongStructure/archive/v0.2.tar.gz',   
+  download_url = 'https://github.com/zhiweigan/pySongStructure/archive/v0.3.tar.gz',   
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
   install_requires=[            
           'numpy',
           'librosa',
+          'scipy==1.2',
           'msaf',
-          'scipy<=1.2'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',     
